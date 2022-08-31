0f44e96ea7253026e93479b2bcf72c62b3c780e7
nomad <b.quentin@protonmail.com>
2022-08-31 16:35:50 +0200

    Premiére installation de magento fonctionnel

--- ias/terraform/main.tf
+++ ias/terraform/main.tf
@@ -183,7 +183,59 @@ resource "azurerm_storage_account" "magento-storage" {
     virtual_network_subnet_ids = [azurerm_subnet.subnet_bdd.id]
   }
 }
+# Create network interface 2 pour elastic
+resource "azurerm_network_interface" "nic_elasticsrh" {
+  name                = "nic_elasticsrh"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  ip_configuration {
+    name                          = "nic_elastic_config"
+    subnet_id                     = azurerm_subnet.subnet_elastic.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ipelastic.id
+  }
+}
 
+# Create virtual machine for elastic search
+resource "azurerm_linux_virtual_machine" "vmelasticsrh" {
+  name                  = "vm_elasticsrh"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic_elasticsrh.id]
+  size                  = "Standard_DS1_v2"
+
+  os_disk {
+    name                 = "myOsDiskelastic"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+  custom_data = data.template_cloudinit_config.configelastic.rendered
+  computer_name                   = "elastic"
+  admin_username                  = "elastic"
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "elastic"
+    public_key = azurerm_ssh_public_key.ssh_nomad.public_key
+  }
+}
+
+#rule VM autorization
+resource "azurerm_mariadb_firewall_rule" "mdbrule" {
+  name = "rule_magento_db"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name = azurerm_mariadb_server.server_magento.name
+
+  start_ip_address = azurerm_public_ip.public_ipapp.ip_address
+  end_ip_address = azurerm_public_ip.public_ipapp.ip_address
+}
 
 # Create virtual machine
 resource "azurerm_linux_virtual_machine" "myterraformvm" {
@@ -298,57 +350,6 @@ resource "azurerm_application_gateway" "network_gateway" {
     backend_http_settings_name = local.http_setting_name
   }
 }
-# Create network interface 2 pour elastic
-resource "azurerm_network_interface" "nic_elasticsrh" {
-  name                = "nic_elasticsrh"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  ip_configuration {
-    name                          = "nic_elastic_config"
-    subnet_id                     = azurerm_subnet.subnet_elastic.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.public_ipelastic.id
-  }
-}
-
-# Create virtual machine for elastic search
-resource "azurerm_linux_virtual_machine" "vmelasticsrh" {
-  name                  = "vm_elasticsrh"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.nic_elasticsrh.id]
-  size                  = "Standard_DS1_v2"
-
-  os_disk {
-    name                 = "myOsDiskelastic"
-    caching              = "ReadWrite"
-    storage_account_type = "Premium_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-  custom_data = data.template_cloudinit_config.configelastic.rendered
-  computer_name                   = "elastic"
-  admin_username                  = "elastic"
-  disable_password_authentication = true
-
-  admin_ssh_key {
-    username   = "elastic"
-    public_key = azurerm_ssh_public_key.ssh_nomad.public_key
-  }
-}
 
 
-#rule VM autorization
-resource "azurerm_mariadb_firewall_rule" "mdbrule" {
-  name = "rule_magento_db"
-  resource_group_name = azurerm_resource_group.rg.name
-  server_name = azurerm_mariadb_server.server_magento.name
 
-  start_ip_address = azurerm_public_ip.public_ipapp.ip_address
-  end_ip_address = azurerm_public_ip.public_ipapp.ip_address
-}
